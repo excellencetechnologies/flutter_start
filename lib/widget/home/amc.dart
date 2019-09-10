544b474354a4de829e01ad5a7978bbcb1bf71826
etech <email@gmail.com>
2019-09-10 11:18:43 +0530

    json testing with model

--- lib/widget/home/amc.dart
+++ lib/widget/home/amc.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:myapp/model/user.dart';
+// class MyButton extends StatefulWidget {
+//   String jsonString =
+//       "{\"name\": \"John Smith\",\"email\": \"john@example.com\"}";
+
+//   void _parseJSON() {
+//     setState() {}
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FlatButton(
+//       child: Text("Test JSON"),
+//       onPressed: () {
+//         _parseJSON();
+//       },
+//     );
+//   }
+// }
 
 class AMCSummary extends StatelessWidget {
   @override
@@ -21,11 +41,26 @@ class AMCSummary extends StatelessWidget {
                 Text("21 AMC's in DB"),
                 Text("15 AMC's in DB"),
                 Text("17th July, last amc cron pass"),
+                FlatButton(
+                  child: Text("Test JSON"),
+                  onPressed: () {
+                    String jsonString =
+                        "{\"name\": \"John Smith\",\"email\": \"john@example.com\"}";
+
+                    User user = User.fromJson(jsonDecode(jsonString));
+
+                    print('Howdy, ${user.name}!');
+                    print('We sent the verification link to ${user.email}.');
+                  },
+                )
               ],
             ),
           ),
           Flexible(
-            child: Icon(Icons.info, color: Theme.of(context).primaryColorLight,),
+            child: Icon(
+              Icons.info,
+              color: Theme.of(context).primaryColorLight,
+            ),
           )
         ],
       ),
